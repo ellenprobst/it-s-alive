b9820f96e961e6670eb9ac6162878de4cdeff8a2
Ellen Probst <ellenprobst@hotmail.com>
2017-10-09 12:03:39 -0400

    update blender file/add helpers

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 var renderer, scene, camera, group;
 var mouseX = 0;
 var mouseY= 0;
+
+var skull, eye;
    
 init();
 animate();  
@@ -15,28 +17,50 @@ function init() {
 	document.body.appendChild(renderer.domElement);
   
 	// camera
-	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-	camera.position.set(0, 100, 500);
+	camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, .1, 10000);
+	camera.position.set(0, 0, 60);
+	//camera.rotation.set(-14.56,-85.02,-14.50);
+
        
             
 	// scene
 	scene = new THREE.Scene();
 	scene.updateMatrixWorld();
-  
-	var light = new THREE.AmbientLight( 0x999999, .8 ); 
-	scene.add( light );
-	var hemilight = new THREE.HemisphereLight( "#D66D75", "#26688F", 1.7 ); 
-	scene.add( hemilight ); 
-	var dirLight = new THREE.DirectionalLight( 0xffffff, .6 );
-		dirLight.color.setHSL( 0.1, 1, 0.95 );
-		dirLight.position.set( -1, 1.75, 1 );
-		dirLight.position.multiplyScalar( 50 ); 
-		scene.add( dirLight );
 
+
+	var light = new THREE.SpotLight( 16726440, 1 );
+	light.distance = 0;
+	light.angle = 0.314159;
+	light.decay = 1;
+	light.position.set( -17.56, -21.69, 55.41 );
+	scene.add( light );
+	
+	// var sphereSize = 10;
+	// var spotLightHelper = new THREE.SpotLightHelper( light, sphereSize );
+	// scene.add( spotLightHelper );
+
+	var pointLight = new THREE.PointLight( 216285, 3.1 );
+	pointLight.distance = 0;
+	pointLight.decay = 1;
+	pointLight.position.set( -2.37, -16.15, 15.48 );
+	scene.add( pointLight );
+
+	 var sphereSize = 1;
+	 var pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
+	 scene.add( pointLightHelper );
+
+	 var size = 100;
+var divisions = 10;
+
+var gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
 
 	//load mesh
-  var loader = new THREE.JSONLoader(); 
-  loader.load('https://raw.githubusercontent.com/ellenprobst/it-s-alive/master/blender/skulleyes.json', generateMesh );
+  	var loader = new THREE.JSONLoader(); 
+  	loader.load('https://raw.githubusercontent.com/ellenprobst/it-s-alive/master/blender/skull.json', generateMesh );
+
+  	var loader = new THREE.JSONLoader(); 
+  	loader.load('https://raw.githubusercontent.com/ellenprobst/it-s-alive/master/blender/eyes.json', generateEyes );
 
 	window.addEventListener( 'resize', onWindowResize, false );
  
@@ -47,12 +71,25 @@ function init() {
 
 
 
-// load pencil 
+// load skull 
 function generateMesh(geometry, material){
-		geometry.computeVertexNormals();
+	geometry.computeVertexNormals();
 
 	skull = new THREE.Mesh(geometry, material);
+	skull.scale.y = skull.scale.z = skull.scale.x = 8.37;
+
 	scene.add( skull );
+}; 
+
+// load skull 
+function generateEyes(geometry, material){
+	geometry.computeVertexNormals();
+
+	eye = new THREE.Mesh(geometry, material);
+	eye.scale.y = eye.scale.z = eye.scale.x = 8;
+	eye.position.set(-4.46,-0.22,0.12);
+
+	scene.add( eye );
 }; 
 
 
@@ -81,6 +118,8 @@ function render() {
 function animate() {
 	
 	requestAnimationFrame( animate );
+	scene ? scene.rotation.y = mouseX * 1.5 : null;
+	scene ? scene.rotation.x = mouseY * 1.5 : null;
 
 	render();
 }; 
