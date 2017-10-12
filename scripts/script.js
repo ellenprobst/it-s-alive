fea7b1012bad233eb452f28abaa90b8b22bc1471
Ellen Probst <ellenprobst@hotmail.com>
2017-10-11 22:04:38 -0400

    add text / group skull

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,10 @@
-var renderer, scene, camera, group;
+var renderer, scene, camera, group, textMesh;
 var mouseX = 0;
 var mouseY= 0;
 
-var skull, leftEye, rightEye, light1, light2, light3, light4;
-   
+var skull, leftEye, rightEye, light1, light2, light3, light4, light5, light6, light7, light8;
+  
+    
 init();
 animate();  
 
@@ -33,8 +34,8 @@ function init() {
 	light.position.set( -50.56, -21.69, 50.41 );
 	scene.add( light );
 	
-		 var sphereSize = 10;
-		 var spotLightHelper = new THREE.SpotLightHelper( light, sphereSize );
+		 // var sphereSize = 10;
+		 // var spotLightHelper = new THREE.SpotLightHelper( light, sphereSize );
 		 //scene.add( spotLightHelper );
 
 	var pointLight = new THREE.PointLight( 216285, 3.1 );
@@ -46,19 +47,30 @@ function init() {
 		//  var pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
 		//  scene.add( pointLightHelper );
 		var sphere = new THREE.SphereGeometry( 0.1, 16, 8 );
-	light1 = new THREE.PointLight( 0xff0040, .8, 10 );
-	light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0xff0040 } ) ) );
+	light1 = new THREE.PointLight( 16726440, .8, 10 );
+	light1.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 16726440 } ) ) );
 	scene.add( light1 );
-	light2 = new THREE.PointLight( 0x0040ff, .8, 10 );
-	light2.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x0040ff } ) ) );
+	light2 = new THREE.PointLight( 16726440, .8, 10 );
+	light2.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 16726440 } ) ) );
 	scene.add( light2 );
-	light3 = new THREE.PointLight( 0x80ff80, .8, 10 );
-	light3.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x80ff80 } ) ) );
+	light3 = new THREE.PointLight( 16726440, .8, 10 );
+	light3.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 16726440 } ) ) );
 	scene.add( light3 );
-	light4 = new THREE.PointLight( 0x80ff80, .8, 10 );
-	light4.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 0x80ff80 } ) ) );
+	light4 = new THREE.PointLight( 16726440, .8, 10 );
+	light4.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 16726440 } ) ) );
 	scene.add( light4 );
-	
+		light5 = new THREE.PointLight( 16726440, .8, 10 );
+	light5.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 16726440 } ) ) );
+	scene.add( light5 );
+		light6 = new THREE.PointLight( 16726440, .8, 10 );
+	light6.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 16726440 } ) ) );
+	scene.add( light6 );
+		light7 = new THREE.PointLight( 16726440, .8, 10 );
+	light7.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: 16726440 } ) ) );
+	scene.add( light7 );
+	light8 = new THREE.PointLight( '#87F5FB', .8, 10 );
+	light8.add( new THREE.Mesh( sphere, new THREE.MeshBasicMaterial( { color: '#87F5FB' } ) ) );
+	scene.add( light8 );
 
 
 	// grid
@@ -69,25 +81,63 @@ function init() {
 
 	//load mesh
   	var loader = new THREE.JSONLoader(); 
-  	loader.load('https://raw.githubusercontent.com/ellenprobst/it-s-alive/master/blender/skull.json', generateMesh );
+  	loader.load('https://raw.githubusercontent.com/ellenprobst/it-s-alive/master/blender/skull.json', generateSkull );
   	loader.load('https://raw.githubusercontent.com/ellenprobst/it-s-alive/master/blender/eyes.json', generateLeftEye );
   	loader.load('https://raw.githubusercontent.com/ellenprobst/it-s-alive/master/blender/eyes.json', generateRightEye );
   	
-	window.addEventListener( 'resize', onWindowResize, false );
   
+  	group = new THREE.Group();
+  	group.position.x = 2;
+	scene.add( group );
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+  	// load text
+  	generateText();
+
 };
 
 
+function generateText(){
+	var loader = new THREE.FontLoader();
+	loader.load( '../scripts/optimer_regular.typeface.json', function ( font ) {
+
+	var textGeometry = new THREE.TextGeometry( "It's alive !", {
+	    font: font,
+
+	    size: 6,
+	    height: 5,
+	    curveSegments: 20,
+
+	    bevelThickness: 1,
+	    bevelSize: 1,
+	    bevelEnabled: false
+
+	  });
+
+	var textMaterial = new THREE.MeshPhongMaterial( 
+	    { color: 16726440, specular: 0xffffff }
+	);
+
+	var mesh = new THREE.Mesh( textGeometry, textMaterial );
+	mesh.scale.z =mesh.scale.y=mesh.scale.x=.3;
+	mesh.position.y = -10;
+	mesh.position.x = -5;
+	  
+	scene.add( mesh );
+	});
+
+}; 
+
 // generate skull 
-function generateMesh(geometry, material){
+function generateSkull(geometry, material){
 	geometry.computeVertexNormals();
 
 	skull = new THREE.Mesh(geometry, material);
 	skull.scale.y = skull.scale.z = skull.scale.x = 8.37;
 
-	scene.add( skull );
+	group.add( skull );
 }; 
-
 
 // generate eye 
 function generateLeftEye(geometry, material){
@@ -95,13 +145,13 @@ function generateLeftEye(geometry, material){
 	geometry.center();
 	
 	leftEye = new THREE.Mesh(geometry, material);
-	leftEye.scale.y = leftEye.scale.z = leftEye.scale.x = 8;
+	leftEye.scale.y = leftEye.scale.z = leftEye.scale.x = 8.5;
 	leftEye.position.set(-4.5,1.7,4.3);
 
 	// var box = new THREE.BoxHelper( eye, 0xffff00 );
 	// scene.add( box );
 
-	scene.add( leftEye );
+	group.add( leftEye );
 };
 
 // generate eye 
@@ -110,13 +160,13 @@ function generateRightEye(geometry, material){
 	geometry.center();
 	
 	rightEye = new THREE.Mesh(geometry, material);
-	rightEye.scale.y = rightEye.scale.z = rightEye.scale.x = 8;
+	rightEye.scale.y = rightEye.scale.z = rightEye.scale.x = 8.5;
 	rightEye.position.set(0,1.7,4.3);
 
 	// var box = new THREE.BoxHelper( eye, 0xffff00 );
 	// scene.add( box );
 
-	scene.add( rightEye );
+	group.add( rightEye );
 }; 
 
 
@@ -148,9 +198,9 @@ function animate(event) {
 	
 	requestAnimationFrame( animate );
 
-	if (scene) {
-		scene.rotation.y = mouseX * .15;
-		scene.rotation.x = mouseY * -.15;
+	if (group) {
+		group.rotation.y = mouseX * .15;
+		group.rotation.x = mouseY * -.15;
 	}
 
 	if (rightEye) {
@@ -160,20 +210,32 @@ function animate(event) {
 
 	var time = Date.now() * 0.0008;
 	
-				light1.position.x = Math.sin( time * 0.7 ) * 20;
-				light1.position.y = Math.cos( time * 0.5 ) * 20;
+				light1.position.x = Math.sin( time * 0.7 ) * 10;
+				light1.position.y = Math.cos( time * 0.5 ) * 15;
 				light1.position.z = Math.cos( time * 0.3 ) * 10;
-				light2.position.x = Math.cos( time * 0.3 ) * 20;
-				light2.position.y = Math.sin( time * 0.5 ) * 20;
+				light2.position.x = Math.cos( time * 0.3 ) * 15;
+				light2.position.y = Math.sin( time * 0.5 ) * 15;
 				light2.position.z = Math.sin( time * 0.7 ) * 10;
-				light3.position.x = Math.sin( time * 0.7 ) * 20;
-				light3.position.y = Math.cos( time * 0.3 ) * 20;
-				light3.position.z = Math.sin( time * 0.5 ) * 10;
-				light4.position.x = Math.sin( time * 0.5 ) * 20;
-				light4.position.y = Math.cos( time * 0.3 ) * 20;
+				light3.position.x = Math.sin( time * 0.7 ) * 10;
+				light3.position.y = Math.cos( time * 0.3 ) * 15;
+				light3.position.z = Math.sin( time * 0.5 ) * 15;
+				light4.position.x = Math.sin( time * 0.5 ) * 10;
+				light4.position.y = Math.cos( time * 0.3 ) * 15;
 				light4.position.z = Math.sin( time * 0.7 ) * 10;
+				light5.position.x = Math.sin( time * 0.6 ) * 15;
+				light5.position.y = Math.cos( time * 0.7 ) * 10;
+				light5.position.z = Math.cos( time * 0.4 ) * 15;
+				light6.position.x = Math.cos( time * 0.4 ) * 10;
+				light6.position.y = Math.sin( time * 0.5 ) * 15;
+				light6.position.z = Math.sin( time * 0.7 ) * 10;
+				light7.position.x = Math.sin( time * 0.5 ) * 10;
+				light7.position.y = Math.cos( time * 0.3 ) * 10;
+				light7.position.z = Math.sin( time * 0.6 ) * 15;
+				light8.position.x = Math.sin( time * 0.7 ) * 10;
+				light8.position.y = Math.cos( time * 0.3 ) * 15;
+				light8.position.z = Math.sin( time * 0.5 ) * 15;
 				
 	render();
 }; 
  
-// to do: remove shininess, add group for skull, change light colors
+// to do: remove shininess,  change light colors
